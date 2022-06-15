e72f3ad2778d2dd8d5a3afffa42f9af602daa5ce
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-06-14 20:29:57 -0400

    Completed first challenge

---
+++ COMPLETE - COCI '09 Contest 1 #1 Note.py
@@ -0,0 +1,29 @@
+# https://dmoj.ca/problem/coci09c1p1
+
+i = input()
+
+temp = 0
+_type = ""
+final = ""
+
+for x in i.split(" "):
+    if temp:
+        if final:
+            if _type == final:
+                answer = _type
+
+        if int(x) - 1 == temp:
+            _type = "ascending"
+            if not final:
+                final = "ascending"
+        elif int(x) + 1 == temp:
+            _type = "descending"
+            if not final:
+                final = "descending"
+        else:
+            answer = "mixed"
+            break
+
+    temp = int(x)
+
+print(answer)
